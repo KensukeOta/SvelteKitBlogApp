08f9bb6b143bdd261cd637e5393851a07b82d499
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-04-08 09:16:31 +0900

    ユーザー登録処理を実装 (#1)

---
+++ src/routes/(guest)/signup/+page.server.ts
@@ -0,0 +1,37 @@
+import type { Actions } from "./$types";
+import { fail, redirect } from "@sveltejs/kit";
+
+export const actions: Actions = {
+  default: async ({ fetch, request }) => {
+    const values = await request.formData();
+    let errors;
+
+    const name = values.get("name");
+    const email = values.get("email");
+    const password = values.get("password");
+    const password_confirmation = values.get("password_confirmation");
+  
+    try {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/v1/users`, {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password, password_confirmation })
+      });
+      if (!res.ok) {
+        errors = await res.json();
+        throw new Error(errors.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (errors) {
+      return fail(400, { name, email, errors });
+    }
+
+    throw redirect(303, "/");
+  }
+};

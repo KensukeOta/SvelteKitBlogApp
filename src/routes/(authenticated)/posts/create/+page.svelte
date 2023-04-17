99014bf6b20ad0fc9dd3a3bb9ac726628c58cd27
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-04-18 00:18:50 +0900

    記事投稿フォームを実装 (#5)

--- src/routes/(authenticated)/posts/create/+page.svelte
+++ src/routes/(authenticated)/posts/create/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import type { ActionData, PageData } from "./$types";
+	import PostForm from "$lib/components/organisms/PostForm.svelte";
 
+  export let data: PageData;
+
+  export let form: ActionData;
 </script>
 
 <svelte:head>
   <title>記事投稿フォーム - SvelteKitBlogApp</title>
 </svelte:head>
+
+<PostForm title={form?.title ?? ""} body={form?.body ?? ""} errors={form?.errors ?? ""} user={data.user}  />

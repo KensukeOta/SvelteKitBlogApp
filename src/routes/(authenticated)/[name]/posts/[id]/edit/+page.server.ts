c84f4a66b63da87da615198ae5da177b87b36db8
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-04-18 13:38:40 +0900

    記事更新処理を実装 (#11)

--- src/routes/(authenticated)/[name]/posts/[id]/edit/+page.server.ts
+++ src/routes/(authenticated)/[name]/posts/[id]/edit/+page.server.ts
@@ -26,3 +26,38 @@ export const load: PageServerLoad = (async ({ fetch, locals, params }) => {
     post,
   };
 })
+
+export const actions: Actions = {
+  default: async ({ cookies, fetch, params, request }) => {
+    const data = await request.formData();
+    let errors;
+    
+    const title = data.get("title");
+    const body = data.get("body");
+    const user_id = data.get("user_id");
+
+    try {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/v1/posts/${params.id}`, {
+        method: "PATCH",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "X-XSRF-TOKEN": cookies.get("XSRF-TOKEN") ?? "",
+        },
+        body: JSON.stringify({ title, body, user_id })
+      });
+      if (!res.ok) {
+        errors = await res.json();
+        throw new Error(errors.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (errors) {
+      return fail(400, { title, body, errors });
+    }
+
+    throw redirect(303, "/");
+  },
+};

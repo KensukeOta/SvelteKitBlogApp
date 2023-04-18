b205728ab31c3603815d686a3b851e0c6b29bf16
Kensuke Ota <40653830+KensukeOta@users.noreply.github.com>
2023-04-18 12:19:55 +0900

    記事詳細表示機能を実装 (#10)

--- src/routes/(guest)/[name]/posts/[id]/+page.server.ts
+++ src/routes/(guest)/[name]/posts/[id]/+page.server.ts
@@ -1,27 +1,27 @@
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = (async ({ fetch, locals }) => {
-  const user = locals.user;
+export const load: PageServerLoad = (async ({ fetch, locals, params }) => {
+  const user = locals.user
   let res;
-  let posts;
+  let post;
 
   try {
-    res = await fetch(`${import.meta.env.VITE_API_URL}/v1/posts`, {
+    res = await fetch(`${import.meta.env.VITE_API_URL}/v1/posts/${params.id}`, {
       headers: {
         "Accept": "application/json",
       },
     });
     if (!res.ok) {
       const errors = await res.json();
-      throw new Error(errors.message);
+      throw new Error(errors.message)
     }
-    posts = await res.json();
+    post = await res.json();
   } catch (error) {
     console.log(error);
   }
-
+  
   return {
     user,
-    posts,
+    post,
   };
 })
---
+++ src/routes/(guest)/[name]/posts/[id]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+  export let data: PageData
+</script>
+
+<svelte:head>
+  <title>{data.post.title} - SvelteKitBlogApp</title>
+</svelte:head>
+
+<h1 class="font-bold">{data.post.title}</h1>
+
+<p class="text-base whitespace-pre-line">{data.post.body}</p>
+

1e3f9bbbadf89fcdb4c0b182e4fbbdf69c242455
Kensuke Ota <40653830+KensukeOta@users.noreply.github.com>
2023-04-26 16:44:52 +0900

    いいね機能を実装 (#19)

--- src/lib/types/Post.ts
+++ src/lib/types/Post.ts
@@ -1,9 +1,12 @@
 import type { User } from "./User";
+import type { Like } from "./Like";
 
 export interface Post {
   id: number;
   title: string;
   body: string;
   user_id: number;
-  user: User
+  likes_count: number;
+  user: User;
+  likes: Like[]
 }

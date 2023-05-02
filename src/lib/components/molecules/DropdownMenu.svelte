29cd42695393f8b01ccb30524aeed140bb3d2caa
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-05-03 01:56:02 +0900

    いいねした記事の一覧ページを作成し、リンクを実装 (#20)

--- src/lib/components/molecules/DropdownMenu.svelte
+++ src/lib/components/molecules/DropdownMenu.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import { page } from "$app/stores";
 	import LogoutButton from "../atoms/LogoutButton.svelte";
 </script>
 
 <ul class="absolute top-9 right-0 bg-white border cursor-auto py-1 rounded-md text-center w-40">
+  <li><a href={`/${$page.data.user.name}/likes`}>いいねした記事</a></li>
   <hr class="my-1.5">
   <li><LogoutButton /></li>
 </ul>
---
+++ src/routes/(authenticated)/[name]/likes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+  <title>いいねした記事一覧 - SvelteKitBlogApp</title>
+</svelte:head>
+

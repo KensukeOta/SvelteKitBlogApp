e4e236788e572110b13e5d9158d2ad20642b8408
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-06-02 10:46:20 +0900

    トップページへ遷移したら、1ページ目の記事が表示されるよう修正 (#25)

--- src/routes/(guest)/+page.svelte
+++ src/routes/(guest)/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import type { Post } from "$lib/types/Post";
+  import { firstPage } from "$lib/stores/firstPage";
 	import PostLinkButton from "$lib/components/atoms/PostLinkButton.svelte";
 	import PostItem from "$lib/components/organisms/PostItem.svelte";
 	import Pagination from "$lib/components/molecules/Pagination.svelte";
@@ -12,12 +13,12 @@
   let paginatedPosts: Post[]; // ページネーションされた記事の配列
 
   const changePage = (page: number) => {
-    currentPage = page; // 現在のページ番号を更新
+    firstPage.set(page); // 現在のページ番号を更新
   };
 
   // 記事一覧のページングを実現するためのcomputed関数
   $: {
-    const startIndex = (currentPage - 1) * perPage; // 記事配列の開始インデックスを計算
+    const startIndex = ($firstPage - 1) * perPage; // 記事配列の開始インデックスを計算
     const endIndex = startIndex + perPage;  // 記事配列の終了インデックスを計算
     paginatedPosts = data.posts.slice(startIndex, endIndex);  // 開始インデックスから終了インデックスまでの記事を抽出
   }
@@ -42,7 +43,7 @@
 </section>
 
 <Pagination
-    currentPage={currentPage}
+    currentPage={$firstPage}
     perPage={perPage}
     totalItems={data.posts.length}
     onPageChange={changePage}

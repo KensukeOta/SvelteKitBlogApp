9311c37e95c0bb96202116c06e7693a6e35b9380
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-06-14 12:25:00 +0900

    記事投稿フォームにマークダウン機能を実装 (#27)

---
+++ src/lib/components/organisms/MarkdownArea.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Markdown from "svelte-exmarkdown";
+  import { gfmPlugin } from "svelte-exmarkdown/gfm";
+  
+  export let md: any;
+</script>
+
+<div class="markdown-body flex h-[calc(100%-140px)]">
+  <textarea bind:value={md} name="body" id="body" placeholder="マークダウン記法で記述することができます。" class="flex-1 bg-gray-200 w-1/2 overflow-y-auto p-2" />
+
+  <div class="flex-1 w-1/2 px-4">
+    <Markdown {md} plugins={[gfmPlugin]} />
+  </div>
+</div>

5869875728100eb917420131112fd28bdd8b837a
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-06-14 13:31:49 +0900

    記事詳細ページにマークダウン記法で書かれた記事をレンダリングするよう修正 (#27)

--- src/routes/(guest)/[name]/posts/[id]/+page.svelte
+++ src/routes/(guest)/[name]/posts/[id]/+page.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
+  import "github-markdown-css/github-markdown.css";
+  import Markdown from "svelte-exmarkdown";
+  import { gfmPlugin } from "svelte-exmarkdown/gfm";
+  import Avatar from "$lib/components/atoms/Avatar.svelte";
   import LikeArea from "$lib/components/molecules/LikeArea.svelte";
 
-  export let data: PageData
+  export let data: PageData;
+
+  const avatarProps = {
+    src: data.post.user.avatar ? data.post.user.avatar : "/noavatar.jpeg",
+    className: "inline-block border h-8 mx-1 w-8 object-cover rounded-full"
+  };
 </script>
 
 <svelte:head>
   <title>{data.post.title} - SvelteKitBlogApp</title>
 </svelte:head>
 
-<h1 class="font-bold">{data.post.title}</h1>
-
-<p class="text-base whitespace-pre-line">{data.post.body}</p>
+<section class="p-8">
+  <h1 class="font-bold text-4xl">{data.post.title}</h1>
+  <p class="mt-1">by <a href={`/${data.post.user.name}`} class="hover:underline"><Avatar {...avatarProps} />{data.post.user.name}</a></p>
+  
+  <div class="markdown-body mt-16">
+    <Markdown md={data.post.body} plugins={[gfmPlugin]} />
+  </div>
+</section>
 
 <LikeArea post={data.post} />

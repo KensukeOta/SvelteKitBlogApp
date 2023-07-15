eacba0c07012e6a1abfd0b18ab086c9e48f8f66d
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-07-15 20:38:50 +0900

    svelte-tags-inputをインストールし、記事投稿フォームに追加し、タグ投稿処理を実装 (#33)

--- src/routes/(authenticated)/posts/create/+page.svelte
+++ src/routes/(authenticated)/posts/create/+page.svelte
@@ -2,6 +2,7 @@
   import type { SubmitFunction } from "@sveltejs/kit";
 	import type { ActionData, PageData } from "./$types";
 	import { enhance } from "$app/forms";
+  import Tags from "svelte-tags-input";
 	import Input from "$lib/components/atoms/Input.svelte";
 	import MarkdownArea from "$lib/components/organisms/MarkdownArea.svelte";
 
@@ -10,6 +11,8 @@
   export let form: ActionData;
 
   let md = form?.body ?? "";
+
+  let tags: any[] = [];
 
   const handleSubmit = (({ submitter }) => {
     submitter?.setAttribute("disabled", "true");
@@ -48,7 +51,21 @@
     <p class="text-red-500">{form.errors.message}</p>
   {/if}
   <Input {...titleProps} />
+  <div class="my-custom-class mb-2">
+    <Tags bind:tags={tags} addKeys={[32]} maxTags={5} onlyUnique={true} placeholder={"(任意) タグを入力してください。スペース区切りで5つまで入力できます。"} />
+    <input type="hidden" name="tags" bind:value={tags} />
+  </div>
   <MarkdownArea {md} />
   <Input {...userIdProps} />
   <button type="submit" class="block border bg-blue-400 rounded-3xl w-48 mx-auto text-white py-2 mt-1 hover:bg-blue-300">投稿する</button>
 </form>
+
+<style>
+  .my-custom-class :global(.svelte-tags-input-tag) {
+    background:blue;
+  }
+							
+  .my-custom-class :global(.svelte-tags-input-layout) {
+    background:yellow;
+  }
+</style>
